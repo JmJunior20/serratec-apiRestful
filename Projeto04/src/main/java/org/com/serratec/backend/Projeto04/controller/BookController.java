9541751ce109c16775d9d3edd817bd460507dfc5
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-06-02 09:41:16 -0300

    Parte inicial do projeto

---
+++ Projeto04/src/main/java/org/com/serratec/backend/Projeto04/controller/BookController.java
@@ -0,0 +1,52 @@
+package org.com.serratec.backend.Projeto04.controller;
+
+import java.util.List;
+
+import org.com.serratec.backend.Projeto04.entity.BookEntity;
+import org.com.serratec.backend.Projeto04.exceptions.BookNotFoundException;
+import org.com.serratec.backend.Projeto04.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/livro")
+public class BookController {
+
+	@Autowired
+	BookService service;
+	
+	@GetMapping
+	public ResponseEntity<List<BookEntity>> getAll() {
+		return new ResponseEntity<List<BookEntity>>(service.findAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<BookEntity> getBuId(@PathVariable long id) throws BookNotFoundException {
+		return new ResponseEntity<BookEntity>(service.findById(id), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<BookEntity> create(@RequestBody BookEntity entity) {
+		return new ResponseEntity<BookEntity>(service.create(entity), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<BookEntity> update(@PathVariable Long id, @RequestBody BookEntity entity) throws BookNotFoundException {
+		return new ResponseEntity<BookEntity>(service.update(id, entity), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> delete(@PathVariable Long id) {
+		service.delete(id);
+		return new ResponseEntity<String>("Deletado com sucesso", HttpStatus.OK);
+	}
+}
---
+++ Projeto04/src/main/java/org/com/serratec/backend/Projeto04/service/BookService.java
@@ -0,0 +1,56 @@
+package org.com.serratec.backend.Projeto04.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.com.serratec.backend.Projeto04.entity.BookEntity;
+import org.com.serratec.backend.Projeto04.exceptions.BookNotFoundException;
+import org.com.serratec.backend.Projeto04.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+	
+	@Autowired
+	BookRepository repository;
+	
+	public List<BookEntity> findAll() {
+		return repository.findAll();
+	}
+	
+	public BookEntity findById(Long id) throws BookNotFoundException {
+		Optional<BookEntity> book = repository.findById(id);
+		if(book.isEmpty()) {
+			throw new BookNotFoundException("O livro não foi encontrado com esse id: " + id);
+		}
+		return book.get();
+	}
+	
+	public BookEntity create(BookEntity entity) {
+		return repository.save(entity);
+	}
+	
+	public BookEntity update(Long id, BookEntity entity) throws BookNotFoundException {
+		BookEntity book = this.findById(id);
+		if(entity.getTitulo() != null) {
+			book.setTitulo(entity.getTitulo());
+		}
+		if(entity.getAutor() != null) {
+			book.setAutor(entity.getAutor());
+		}
+		if(entity.getTipo() != null) {
+			book.setTipo(entity.getTipo());
+		}
+		if(entity.getData() != null) {
+			book.setData(entity.getData());
+		}
+		
+		return repository.save(book);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+}
